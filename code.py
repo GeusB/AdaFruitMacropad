8985e61f65d52aec328b08087b319157f64a3c78
Geuskens Ben <ben.geuskens@cegeka.com>
2022-07-22 22:36:24 +0200

    next layer

--- code.py
+++ code.py
@@ -22,130 +22,253 @@ class Colors:
     LightBlue = (0, 255, 255)
     Blue = (0, 0, 255)
     Pink = (255, 0, 255)    
-    Black = (0,0,0)    
+    Black = (0,0,0)
 
-while True:   
-        
-    name = 'Home'
-    macros = [
-        (Colors.Red, 'Ctrl+X', [Keycode.CONTROL, 'x']),
-        (Colors.Red, 'Ctrl+C', [Keycode.CONTROL, 'c']),
-        (Colors.Red, 'Ctrl+V', [Keycode.CONTROL, 'v']),
-        (Colors.Red, 'WinTab', [Keycode.WINDOWS, Keycode.TAB]), 
-        (Colors.Orange, 'Ctrl+W', [Keycode.CONTROL, 'W']),
-        (Colors.Blue, 'Ctrl+D', [Keycode.CONTROL, 'D']),
-        (Colors.Green, '<<', [Keycode.COMMAND, 'r']),
-        (Colors.Green, 'Pauze', [Keycode.COMMAND, 'H']),
-        (Colors.Green, '>>', [Keycode.COMMAND, 'N']),
-        (Colors.Yellow, 'Next', [Keycode.COMMAND, 'n', -Keycode.COMMAND, 'www.adafruit.com\n']), 
-        (Colors.LightBlue, 'Mute', [Keycode.COMMAND, 'n', -Keycode.COMMAND, 'www.digikey.com\n']),
-        (Colors.Pink, 'Prev', [Keycode.COMMAND, 'n', -Keycode.COMMAND, 'www.hackaday.com\n']),
-        # Encoder button
-        (0x000000, '', [Keycode.COMMAND, 'w'])
-    ]
-    macropad.display.auto_refresh = False 
-    macropad.pixels.auto_write = False
+    
 
-    #display setup
-    group = displayio.Group()
-    for key_index in range(12):
-        x = key_index % 3
-        y = key_index // 3
-        group.append(label.Label(terminalio.FONT, text='', color=Colors.White, anchored_position=((macropad.display.width - 1) * x /2,macropad.display.height - 1 -(3 - y) * 12), anchor_point=(x / 2, 1.0)))
-    group.append(Rect(0, 0, macropad.display.width, 12, fill=Colors.White)) 
-    group.append(label.Label(terminalio.FONT, text='', color=Colors.Black, anchored_position=(macropad.display.width//2, -2), anchor_point=(0.5, 0.0)))
-    macropad.display.show(group)
+while True:
+    if current_layer == 1:
+        title = 'Home'
+        macros = [
+            (Colors.Red, 'Ctrl+X', [Keycode.CONTROL, 'x']),
+            (Colors.Red, 'Ctrl+C', [Keycode.CONTROL, 'c']),
+            (Colors.Red, 'Ctrl+V', [Keycode.CONTROL, 'v']),
+            (Colors.Red, 'WinTab', [Keycode.WINDOWS, Keycode.TAB]), 
+            (Colors.Orange, 'Ctrl+W', [Keycode.CONTROL, 'W']),
+            (Colors.Blue, 'Ctrl+D', [Keycode.CONTROL, 'D']),
+            (Colors.Green, '<<', [Keycode.COMMAND, 'r']),
+            (Colors.Green, 'Pauze', [Keycode.COMMAND, 'H']),
+            (Colors.Green, '>>', [Keycode.COMMAND, 'N']),
+            (Colors.Yellow, 'Next', [Keycode.COMMAND, 'n', -Keycode.COMMAND, 'www.adafruit.com\n']), 
+            (Colors.LightBlue, 'Mute', [Keycode.COMMAND, 'n', -Keycode.COMMAND, 'www.digikey.com\n']),
+            (Colors.Pink, 'Prev', [Keycode.COMMAND, 'n', -Keycode.COMMAND, 'www.hackaday.com\n']),
+            #encoder click
+            (0x000000, '', [Keycode.COMMAND, 'w'])
+        ]
+        macropad.display.auto_refresh = False 
+        macropad.pixels.auto_write = False
 
-    # display labels and set leds
-    group[13].text = name 
-    for i in range(12):
-        if i < len(macros): 
-            macropad.pixels[i] = macros[i][0]
-            group[i].text = macros[i][1]
-        else: 
-            macropad.pixels[i] = 0
-            group[i].text = ''
-    macropad.keyboard.release_all() 
-    macropad.consumer_control.release() 
-    macropad.mouse.release_all() 
-    macropad.stop_tone() 
-    macropad.pixels.show() 
-    macropad.display.refresh()
+        #display setup
+        display_group = displayio.Group()
+        for key_index in range(12):
+            x = key_index % 3
+            y = key_index // 3
+            display_group.append(label.Label(terminalio.FONT, text='', color=Colors.White, anchored_position=((macropad.display.width - 1) * x /2,macropad.display.height - 1 -(3 - y) * 12), anchor_point=(x / 2, 1.0)))
+        display_group.append(Rect(0, 0, macropad.display.width, 12, fill=Colors.White)) 
+        display_group.append(label.Label(terminalio.FONT, text='', color=Colors.Black, anchored_position=(macropad.display.width//2, -2), anchor_point=(0.5, 0.0)))
+        macropad.display.show(display_group)
 
-    key_event = macropad.keys.events.get()
+        #display labels and set leds
+        display_group[13].text = title 
+        for i in range(12):
+            if i < len(macros): 
+                macropad.pixels[i] = macros[i][0]
+                display_group[i].text = macros[i][1]
+            else: 
+                macropad.pixels[i] = 0
+                display_group[i].text = ''
+        macropad.keyboard.release_all() 
+        macropad.consumer_control.release() 
+        macropad.mouse.release_all() 
+        macropad.stop_tone() 
+        macropad.pixels.show() 
+        macropad.display.refresh()
 
-    if key_event:
-        if key_event.pressed:
-            if key_event.key_number == 0:
-                macropad.keyboard.press(macropad.Keycode.CONTROL, macropad.Keycode.X)
-                macropad.keyboard.release_all()
-            if key_event.key_number == 1:
-                macropad.keyboard.press(macropad.Keycode.CONTROL, macropad.Keycode.C)
-                macropad.keyboard.release_all()
-            if key_event.key_number == 2:
-                macropad.keyboard.press(macropad.Keycode.CONTROL, macropad.Keycode.V)
-                macropad.keyboard.release_all()
-            if key_event.key_number == 3:
-                macropad.keyboard.press(macropad.Keycode.WINDOWS, macropad.Keycode.TAB)
-                macropad.keyboard.release_all() 
-            if key_event.key_number == 4:
-                macropad.keyboard.press(macropad.Keycode.CONTROL, macropad.Keycode.W)
-                macropad.keyboard.release_all()
-            if key_event.key_number == 5:
-                macropad.keyboard.press(macropad.Keycode.WINDOWS, macropad.Keycode.D)
-                macropad.keyboard.release_all()           
-            if key_event.key_number == 6:
-                macropad.consumer_control.send(
-                    macropad.ConsumerControlCode.SCAN_PREVIOUS_TRACK
-                )
-            if key_event.key_number == 7:
-                macropad.consumer_control.send(
-                    macropad.ConsumerControlCode.PLAY_PAUSE
-                )
-            if key_event.key_number == 8:
-                macropad.consumer_control.send(
-                    macropad.ConsumerControlCode.SCAN_NEXT_TRACK
-                )
-            if key_event.key_number == 9:
-                if current_layer > 1:
-                    current_layer= current_layer - 1
-                else:
-                    current_layer = max_layer
-            if key_event.key_number == 10:
-                macropad.consumer_control.send(
-                    macropad.ConsumerControlCode.MUTE
-                )
-            if key_event.key_number == 11:
-                if current_layer < max_layer:
-                    current_layer = current_layer + 1
-                else:
-                    current_layer = 1
+        key_event = macropad.keys.events.get()
 
-    macropad.encoder_switch_debounced.update()
+        if key_event:
+            if key_event.pressed:
+                if key_event.key_number == 0:
+                    macropad.keyboard.press(macropad.Keycode.CONTROL, macropad.Keycode.X)
+                    macropad.keyboard.release_all()
+                if key_event.key_number == 1:
+                    macropad.keyboard.press(macropad.Keycode.CONTROL, macropad.Keycode.C)
+                    macropad.keyboard.release_all()
+                if key_event.key_number == 2:
+                    macropad.keyboard.press(macropad.Keycode.CONTROL, macropad.Keycode.V)
+                    macropad.keyboard.release_all()
+                if key_event.key_number == 3:
+                    macropad.keyboard.press(macropad.Keycode.WINDOWS, macropad.Keycode.TAB)
+                    macropad.keyboard.release_all() 
+                if key_event.key_number == 4:
+                    macropad.keyboard.press(macropad.Keycode.CONTROL, macropad.Keycode.W)
+                    macropad.keyboard.release_all()
+                if key_event.key_number == 5:
+                    macropad.keyboard.press(macropad.Keycode.WINDOWS, macropad.Keycode.D)
+                    macropad.keyboard.release_all()           
+                if key_event.key_number == 6:
+                    macropad.consumer_control.send(
+                        macropad.ConsumerControlCode.SCAN_PREVIOUS_TRACK
+                    )
+                if key_event.key_number == 7:
+                    macropad.consumer_control.send(
+                        macropad.ConsumerControlCode.PLAY_PAUSE
+                    )
+                if key_event.key_number == 8:
+                    macropad.consumer_control.send(
+                        macropad.ConsumerControlCode.SCAN_NEXT_TRACK
+                    )
+                if key_event.key_number == 9:
+                    if current_layer > 1:
+                        current_layer= current_layer - 1
+                    else:
+                        current_layer = max_layer
+                if key_event.key_number == 10:
+                    macropad.consumer_control.send(
+                        macropad.ConsumerControlCode.MUTE
+                    )
+                if key_event.key_number == 11:
+                    if current_layer < max_layer:
+                        current_layer = current_layer + 1
+                    else:
+                        current_layer = 1
 
-    if macropad.encoder_switch_debounced.pressed:
-        if move == 0:
-            move = 1
-        else:
-            move = 0
+        macropad.encoder_switch_debounced.update()
 
-    if move == 1:
-        macropad.mouse.move(x=+1)
-        macropad.mouse.move(x=-1)
-        # macropad.pixels[11] = Colors.Red
-        
-    current_position = macropad.encoder
+        if macropad.encoder_switch_debounced.pressed:
+            if move == 0:
+                move = 1
+            else:
+                move = 0
 
-    if macropad.encoder > last_position:
-        macropad.consumer_control.send(
-            macropad.ConsumerControlCode.VOLUME_INCREMENT
-        )
-        last_position = current_position
+        if move == 1:
+            macropad.mouse.move(x=+1)
+            macropad.mouse.move(x=-1)
+            macropad.pixels[11] = Colors.Red
+            
+        current_position = macropad.encoder
 
-    if macropad.encoder < last_position:        
-        macropad.consumer_control.send(
-            macropad.ConsumerControlCode.VOLUME_DECREMENT
-        )
-        last_position = current_position
+        if macropad.encoder > last_position:
+            macropad.consumer_control.send(
+                macropad.ConsumerControlCode.VOLUME_INCREMENT
+            )
+            last_position = current_position
 
- # if current_layer == 2:
+        if macropad.encoder < last_position:        
+            macropad.consumer_control.send(
+                macropad.ConsumerControlCode.VOLUME_DECREMENT
+            )
+            last_position = current_position
+
+    if current_layer != 1:
+        title = 'Debug'
+        macros = [
+            (Colors.Red, 'Debug', [Keycode.F5]),
+            (Colors.Red, 'Run', [Keycode.SHIFT, Keycode.F5]),
+            (Colors.Red, 'F9', [Keycode.F9]),
+            (Colors.Red, 'Step', [Keycode.F10]), 
+            (Colors.Orange, 'StepIn', [Keycode.F11]),
+            (Colors.Blue, 'StepOut', [Keycode.SHIFT, Keycode.F11]),
+            (Colors.Green, '<<', [Keycode.COMMAND, 'r']),
+            (Colors.Green, 'Pauze', [Keycode.COMMAND, 'H']),
+            (Colors.Green, '>>', [Keycode.COMMAND, 'N']),
+            (Colors.Yellow, 'Next', [Keycode.COMMAND, 'n', -Keycode.COMMAND, 'www.adafruit.com\n']), 
+            (Colors.LightBlue, 'Mute', [Keycode.COMMAND, 'n', -Keycode.COMMAND, 'www.digikey.com\n']),
+            (Colors.Pink, 'Prev', [Keycode.COMMAND, 'n', -Keycode.COMMAND, 'www.hackaday.com\n']),
+            #encoder click
+            (0x000000, '', [Keycode.WINDOWS, Keycode.TAB])
+        ]
+        macropad.display.auto_refresh = False 
+        macropad.pixels.auto_write = False
+
+        #display setup
+        display_group = displayio.Group()
+        for key_index in range(12):
+            x = key_index % 3
+            y = key_index // 3
+            display_group.append(label.Label(terminalio.FONT, text='', color=Colors.White, anchored_position=((macropad.display.width - 1) * x /2,macropad.display.height - 1 -(3 - y) * 12), anchor_point=(x / 2, 1.0)))
+        display_group.append(Rect(0, 0, macropad.display.width, 12, fill=Colors.White)) 
+        display_group.append(label.Label(terminalio.FONT, text='', color=Colors.Black, anchored_position=(macropad.display.width//2, -2), anchor_point=(0.5, 0.0)))
+        macropad.display.show(display_group)
+
+        #display labels and set leds
+        display_group[13].text = title 
+        for i in range(12):
+            if i < len(macros): 
+                macropad.pixels[i] = macros[i][0]
+                display_group[i].text = macros[i][1]
+            else: 
+                macropad.pixels[i] = 0
+                display_group[i].text = ''
+        macropad.keyboard.release_all() 
+        macropad.consumer_control.release() 
+        macropad.mouse.release_all() 
+        macropad.stop_tone() 
+        macropad.pixels.show() 
+        macropad.display.refresh()
+
+        key_event = macropad.keys.events.get()
+
+        if key_event:
+            if key_event.pressed:
+                if key_event.key_number == 0:
+                    macropad.keyboard.press(macropad.Keycode.CONTROL, macropad.Keycode.X)
+                    macropad.keyboard.release_all()
+                if key_event.key_number == 1:
+                    macropad.keyboard.press(macropad.Keycode.CONTROL, macropad.Keycode.C)
+                    macropad.keyboard.release_all()
+                if key_event.key_number == 2:
+                    macropad.keyboard.press(macropad.Keycode.CONTROL, macropad.Keycode.V)
+                    macropad.keyboard.release_all()
+                if key_event.key_number == 3:
+                    macropad.keyboard.press(macropad.Keycode.WINDOWS, macropad.Keycode.TAB)
+                    macropad.keyboard.release_all() 
+                if key_event.key_number == 4:
+                    macropad.keyboard.press(macropad.Keycode.CONTROL, macropad.Keycode.W)
+                    macropad.keyboard.release_all()
+                if key_event.key_number == 5:
+                    macropad.keyboard.press(macropad.Keycode.WINDOWS, macropad.Keycode.D)
+                    macropad.keyboard.release_all()           
+                if key_event.key_number == 6:
+                    macropad.consumer_control.send(
+                        macropad.ConsumerControlCode.SCAN_PREVIOUS_TRACK
+                    )
+                if key_event.key_number == 7:
+                    macropad.consumer_control.send(
+                        macropad.ConsumerControlCode.PLAY_PAUSE
+                    )
+                if key_event.key_number == 8:
+                    macropad.consumer_control.send(
+                        macropad.ConsumerControlCode.SCAN_NEXT_TRACK
+                    )
+                if key_event.key_number == 9:
+                    if current_layer > 1:
+                        current_layer= current_layer - 1
+                    else:
+                        current_layer = max_layer
+                if key_event.key_number == 10:
+                    macropad.consumer_control.send(
+                        macropad.ConsumerControlCode.MUTE
+                    )
+                if key_event.key_number == 11:
+                    if current_layer < max_layer:
+                        current_layer = current_layer + 1
+                    else:
+                        current_layer = 1
+
+        macropad.encoder_switch_debounced.update()
+
+        if macropad.encoder_switch_debounced.pressed:
+            if move == 0:
+                move = 1
+            else:
+                move = 0
+
+        if move == 1:
+            macropad.mouse.move(x=+1)
+            macropad.mouse.move(x=-1)
+            macropad.pixels[11] = Colors.Red
+            
+        current_position = macropad.encoder
+
+        if macropad.encoder > last_position:
+            macropad.consumer_control.send(
+                macropad.ConsumerControlCode.VOLUME_INCREMENT
+            )
+            last_position = current_position
+
+        if macropad.encoder < last_position:        
+            macropad.consumer_control.send(
+                macropad.ConsumerControlCode.VOLUME_DECREMENT
+            )
+            last_position = current_position
